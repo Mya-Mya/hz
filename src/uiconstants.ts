10946bffcafad78f6c720af55c94c008f74f41db
Mya-Mya <39019907+Mya-Mya@users.noreply.github.com>
2022-12-14 20:43:30 +0900

    Add orange

--- src/uiconstants.ts
+++ src/uiconstants.ts
@@ -5,4 +5,6 @@ export const NORMAL_TEXTSIZE = 24
 export const LARGE_TEXTSIZE = 50
 export const WHITE = () => p.color(255, 255, 255)
 export const LIGHTBLUE = () => p.color(108, 195, 239)
+export const ORANGE = () => p.color(255, 182, 97)
 export const BLACK = () => p.color(0, 0, 0)
+export const brighter = (color: P5.Color) => {
