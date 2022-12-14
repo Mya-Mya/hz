69de41e39d45aa1669998f434280905e75a3f81f
Mya-Mya <39019907+Mya-Mya@users.noreply.github.com>
2022-12-14 18:36:00 +0900

    Create uiconstants.ts

---
+++ src/uiconstants.ts
@@ -0,0 +1,8 @@
+import { p } from "./main"
+export const CANVAS_WIDTH = 1136
+export const CANVAS_HEIGHT = 640
+export const NORMAL_TEXTSIZE = 24
+export const LARGE_TEXTSIZE = 50
+export const WHITE = () => p.color(255, 255, 255)
+export const LIGHTBLUE = () => p.color(108, 195, 239)
+export const BLACK = () => p.color(0, 0, 0)
