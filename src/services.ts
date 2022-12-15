e38cd2c60fa10f0dada6f07f32751fa7029051e5
Mya-Mya <39019907+Mya-Mya@users.noreply.github.com>
2022-12-15 23:29:32 +0900

    Create FadeService

--- src/services.ts
+++ src/services.ts
@@ -1,5 +1,6 @@
 import { View } from "./View"
 import { p } from "./main"
+import { BLACK, CANVAS_HEIGHT, CANVAS_WIDTH } from "./uiconstants"
 class SceneManageService extends View {
     public scene: View
     tick = () => this.scene.tick()
@@ -42,3 +43,52 @@ class ModalManageService extends View {
     }
 }
 export const modal_manage_service = new ModalManageService()
+
+class FadeService extends View {
+    public is_active: boolean = false
+    private fade_type: string
+    private on_done: () => void
+    private s: number
+    private ds: number = 0.02
+
+    tick(): void {
+        if (!this.is_active) return
+        this.s += this.ds
+        if (this.s >= 1) {
+            this.is_active = false
+            if (this.on_done) this.on_done()
+            return
+        }
+
+        p.push()
+        const color = BLACK()
+        switch (this.fade_type) {
+            case "out":
+                color.setAlpha(255 * this.s)
+                break;
+            case "in":
+                color.setAlpha(255 * (1 - this.s))
+                break;
+        }
+        p.fill(color)
+        p.rect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+        p.pop()
+    }
+    start_out(on_done: () => void): void {
+        this.start(on_done, "out")
+    }
+    start_in(on_done: () => void): void {
+        this.start(on_done, "in")
+    }
+    private start(on_done: () => void, fade_type: string) {
+        if (this.is_active) return
+        this.fade_type = fade_type
+        this.s = 0
+        this.is_active = true
+        this.on_done = on_done
+    }
+    mouse_pressed(e: any): boolean {
+        return this.is_active
+    }
+}
+export const fade_service = new FadeService()
