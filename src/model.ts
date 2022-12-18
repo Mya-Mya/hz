7f0cc2e4771d77300bca671d98ceb9fda58a71ec
Mya-Mya <39019907+Mya-Mya@users.noreply.github.com>
2022-12-18 22:21:32 +0900

    Create set_choosing_index method

--- src/model.ts
+++ src/model.ts
@@ -3,3 +3,6 @@ import StoriesManager from "./storyapi/StoriesManager";
 
 export const storiesmanager: StoriesManager = new DummyStoriesManager()
 export let choosing_index: number
+export const set_choosing_index = (index: number): void => {
+    choosing_index = index
+}
